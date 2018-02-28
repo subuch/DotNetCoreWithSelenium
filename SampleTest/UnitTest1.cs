f288f9e28810668b8f18c4dc3e423651270e0a44
Subbu <Subramanian.Ramamoor@CABNB005>
2018-02-28 13:14:31 +1100

    Add project files.

---
+++ SampleTest/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SampleTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
